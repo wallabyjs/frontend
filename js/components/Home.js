9ddd6800f8dc78d65ca906ae126545dc4443cfb3
Marko Grešak <marko@gresak.io>
2015-09-06 03:23:46 +0200

    add styling for tiles
    Current react redering is used just for testinng.
    Styling is split into files for easier managing.

--- js/components/Home.js
+++ js/components/Home.js
@@ -1,19 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as HomeActions from '../actions/HomeActions';
+// import {bindActionCreators} from 'redux';
+// import * as HomeActions from '../actions/HomeActions';
 import styles from '../../styles/app.scss';
 
 class Home extends Component {
   render() {
-    const {title, dispatch} = this.props;
-    const actions = bindActionCreators(HomeActions, dispatch);
+    // const {title, dispatch} = this.props;
+    // const actions = bindActionCreators(HomeActions, dispatch);
+    const tiles = Array.apply(null, {length: 9}).map(() => Array.apply(null, {length: 4}));
     return (
       <main>
-        <h1 className={styles.text}>Welcome {title}!</h1>
-        <button onClick={() => actions.changeTitle('123')}>
-          Update Title
-        </button>
+        <div className={styles.board}>
+          {tiles.map((e, i) => {
+            return (
+              <div className={`${styles.tile} ${styles.rotate0} ${styles[`type${i}`] || ''}`}>
+                {e.map(() => {
+                  return (
+                    <div className={styles.field}>
+                      <div className={styles.borders}></div>
+                    </div>
+                  );
+                })}
+              </div>);
+          })}
+        </div>
       </main>
     );
   }
