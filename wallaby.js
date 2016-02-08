01cd988d72989fe5ffa1e5371120e1ced908ef3a
Artem Govorov <artem.govorov@gmail.com>
2016-02-08 15:31:37 +1000

    Removed compilers section, tsconfig.json is now used

--- wallaby.js
+++ wallaby.js
@@ -6,13 +6,6 @@ module.exports = function (wallaby) {
       '!test/**/test-*.ts*'
     ],
     tests: ['test/**/test-*.ts*'],
-    compilers: {
-      '**/*.ts*': wallaby.compilers.typeScript({
-        typescript: require('typescript'),
-        module: 1,  // commonjs https://github.com/Microsoft/TypeScript/blob/master/src/compiler/types.ts#L2071
-        jsx: 2      // react https://github.com/Microsoft/TypeScript/blob/master/src/compiler/types.ts#L2077
-      })
-    },
 
     env: {
       type: 'node'
